de995137d8d6fec933415a229aa1f2b8f8785d6d
pastel-del <pastelrandolarp@gmail.com>
2026-03-15 03:20:12 -0300

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -51,16 +51,3 @@ alert("Perfil salvo")
 
 }
 
-async function loginDiscord(){
-
-await window.supabaseClient.auth.signInWithOAuth({
-
-provider:"discord",
-
-options:{
-redirectTo: window.location.origin + "/Nome-na-Bio/dashboard.html"
-}
-
-})
-
-}
