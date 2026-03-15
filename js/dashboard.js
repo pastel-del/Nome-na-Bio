3d40c6d68a5e2392e1048d5adab9b14f27496538
pastel-del <pastelrandolarp@gmail.com>
2026-03-15 03:19:22 -0300

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -50,3 +50,17 @@ document.getElementById("youtube").value
 alert("Perfil salvo")
 
 }
+
+async function loginDiscord(){
+
+await window.supabaseClient.auth.signInWithOAuth({
+
+provider:"discord",
+
+options:{
+redirectTo: window.location.origin + "/Nome-na-Bio/dashboard.html"
+}
+
+})
+
+}
