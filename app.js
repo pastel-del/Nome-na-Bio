c015d388240e1879dd2aa34e8eb4e355298ceeec
pastel-del <pastelrandolarp@gmail.com>
2026-03-15 02:46:32 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,75 +1,59 @@
+const SB_URL = "https://zpzdasukgdmfygajnjhn.supabase.co"
+const SB_KEY = "sb_publishable_zFt8CfCxjSbZkZ6QLwKG8A_Vk-Skprf"
 
-const SUPABASE_URL="https://zpzdasukgdmfygajnjhn.supabase.co"
-const SUPABASE_KEY="sb_publishable_zFt8CfCxjSbZkZ6QLwKG8A_Vk-Skprf"
+const client = window.supabase.createClient(SB_URL, SB_KEY)
 
-const supabase=window.supabase.createClient(
-SUPABASE_URL,
-SUPABASE_KEY
-)
+window.login = async function(){
 
-init()
-
-async function init(){
-
-const params=new URLSearchParams(location.search)
-
-const user=params.get("u")
-
-if(user){
-
-loadProfile(user)
+ await client.auth.signInWithOAuth({
+  provider:"discord"
+ })
 
 }
 
-}
+window.logout = async function(){
 
-async function login(){
+ await client.auth.signOut()
 
-await supabase.auth.signInWithOAuth({
-provider:"discord"
-})
+ location.reload()
 
 }
 
 async function loadProfile(username){
 
-document.getElementById("home").classList.add("hidden")
-document.getElementById("profile").classList.remove("hidden")
+ const {data} = await client
+ .from("bios")
+ .select("*")
+ .eq("username",username)
+ .maybeSingle()
 
-const {data}=await supabase
-.from("bios")
-.select("*")
-.eq("username",username)
-.maybeSingle()
+ if(!data) return
 
-if(!data) return
+ document.getElementById("display").innerText = data.display_name
+ document.getElementById("username").innerText = "@"+data.username
+ document.getElementById("bio").innerText = data.bio_text
 
-display.innerText=data.display_name
-username.innerText="@"+data.username
-bio.innerText=data.bio_text
+ document.getElementById("avatar").src =
+ data.pfp_url ||
+ "https://ui-avatars.com/api/?name="+username
 
-avatar.src=data.pfp_url ||
-"https://ui-avatars.com/api/?name="+username
+ const socials = document.getElementById("socials")
 
-views.innerText="👁 "+data.views+" visualizações"
+ if(data.instagram)
+ socials.innerHTML += `<a href="${data.instagram}" target="_blank"><i class="fab fa-instagram"></i></a>`
 
-const socials=document.getElementById("socials")
+ if(data.tiktok)
+ socials.innerHTML += `<a href="${data.tiktok}" target="_blank"><i class="fab fa-tiktok"></i></a>`
 
-if(data.instagram)
-socials.innerHTML+=`<a href="${data.instagram}" target="_blank"><i class="fab fa-instagram"></i></a>`
+ if(data.twitter)
+ socials.innerHTML += `<a href="${data.twitter}" target="_blank"><i class="fab fa-x-twitter"></i></a>`
 
-if(data.tiktok)
-socials.innerHTML+=`<a href="${data.tiktok}" target="_blank"><i class="fab fa-tiktok"></i></a>`
+ if(data.youtube)
+ socials.innerHTML += `<a href="${data.youtube}" target="_blank"><i class="fab fa-youtube"></i></a>`
 
-if(data.twitter)
-socials.innerHTML+=`<a href="${data.twitter}" target="_blank"><i class="fab fa-x-twitter"></i></a>`
+ if(data.facebook)
+ socials.innerHTML += `<a href="${data.facebook}" target="_blank"><i class="fab fa-facebook"></i></a>`
 
-if(data.youtube)
-socials.innerHTML+=`<a href="${data.youtube}" target="_blank"><i class="fab fa-youtube"></i></a>`
-
-if(data.facebook)
-socials.innerHTML+=`<a href="${data.facebook}" target="_blank"><i class="fab fa-facebook"></i></a>`
-
-await supabase.rpc("increment_views",{target_username:username})
+ await client.rpc("increment_views",{target_username:username})
 
 }
