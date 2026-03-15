0f149657df168134f60b1aba8c48db238ceb3b7a
pastel-del <pastelrandolarp@gmail.com>
2026-03-15 02:49:44 -0300

    Update effects.js

--- effects.js
+++ effects.js
@@ -1,1 +1,49 @@
+const canvas=document.getElementById("particles")
 
+const ctx=canvas.getContext("2d")
+
+canvas.width=window.innerWidth
+canvas.height=window.innerHeight
+
+let particles=[]
+
+for(let i=0;i<80;i++){
+
+particles.push({
+
+x:Math.random()*canvas.width,
+y:Math.random()*canvas.height,
+size:Math.random()*2,
+speed:Math.random()*0.5
+
+})
+
+}
+
+function animate(){
+
+ctx.clearRect(0,0,canvas.width,canvas.height)
+
+ctx.fillStyle="white"
+
+particles.forEach(p=>{
+
+ctx.beginPath()
+ctx.arc(p.x,p.y,p.size,0,Math.PI*2)
+ctx.fill()
+
+p.y+=p.speed
+
+if(p.y>canvas.height){
+
+p.y=0
+
+}
+
+})
+
+requestAnimationFrame(animate)
+
+}
+
+animate()
